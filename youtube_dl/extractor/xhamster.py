3927a560ef391ba3813faf745dd9c889b7e2acde
Gabor Miklos <miklosgabor86@gmail.com>
2017-10-05 14:04:01 +0300

    Fixed url replacement regexp

--- youtube_dl/extractor/xhamster.py
+++ youtube_dl/extractor/xhamster.py
@@ -84,7 +84,7 @@ class XHamsterIE(InfoExtractor):
     }]
 
     def _real_extract(self, url):
-        url = re.sub(r'^(https?://.+?\.)?m\.', r'\1', url)
+        url = re.sub(r'^(https?://(?:.+?\.)?)m\.', r'\1', url)
 
         mobj = re.match(self._VALID_URL, url)
         video_id = mobj.group('id') or mobj.group('id_2')
